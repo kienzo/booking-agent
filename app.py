ed2f6e53880ec41f8c6456526c0842f0b77aa6bd
kienzo <kenny_xu@outlook.com.au>
2026-02-26 11:39:04 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("golf_booker_ui.html")
+    return send_from_directory("templates", "golf_booker_ui.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-```
